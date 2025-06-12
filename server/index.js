3dd2d709c383c4fc3e8284793d1b3501abf72384
Ayesh-J <jamadarayesh9@gmail.com>
2025-06-12 13:25:39 +0530

    server mismatch

--- server/index.js
+++ server/index.js
@@ -66,6 +66,6 @@ app.get("/api/test", (req, res) => {
 });
  
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 
